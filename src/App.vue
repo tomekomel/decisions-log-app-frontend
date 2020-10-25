c1dcd83fad305e9a08ec9cf233bdac5727e14705
Tomasz Omeljaniuk <tomekomel@gmail.com>
2020-10-25 10:37:15 +0100

    feat: add create decision component

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,17 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/create">Create</router-link>
     </div>
     <router-view />
   </div>
 </template>
+<style>
+#app {
+  padding: 50px;
+}
+.form-wrapper {
+  width: 500px;
+  margin: 0 auto;
+}
+</style>
--- src/components/decision/Create.vue
+++ src/components/decision/Create.vue
@@ -1,15 +1,86 @@
 <template>
-  <form class="form-horizontal">
-    <div class="form-group">
-      <div class="col-3 col-sm-12">
-        <label class="form-label" for="name">Name</label>
+  <div class="container">
+    <h1>Create New Decision</h1>
+
+    <form class="form-horizontal">
+      <div class="form-group">
+        <div class="col-3 col-sm-12">
+          <label class="form-label" for="title">Title</label>
+        </div>
+        <div class="col-9 col-sm-12">
+          <input
+            class="form-input"
+            type="text"
+            id="title"
+            v-model="title"
+            placeholder="title"
+          />
+        </div>
       </div>
-      <div class="col-9 col-sm-12">
-        <input class="form-input" type="text" id="name" placeholder="Name">
+      <div class="form-group">
+        <div class="col-3 col-sm-12">
+          <label class="form-label" for="reasons">Reasons</label>
+        </div>
+        <div class="col-9 col-sm-12">
+          <textarea
+            class="form-input"
+            id="reasons"
+            v-model="reasons"
+            placeholder="reasons"
+            rows="3"
+          ></textarea>
+        </div>
       </div>
-    </div>
-    <!-- form structure -->
-  </form>
+      <div class="form-group">
+        <div class="col-3 col-sm-12">
+          <label class="form-label" for="author">Author</label>
+        </div>
+        <div class="col-9 col-sm-12">
+          <input
+            class="form-input"
+            type="text"
+            id="author"
+            v-model="author"
+            placeholder="author"
+          />
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-3 col-sm-12">
+          <label class="form-label" for="type">Type</label>
+        </div>
+        <div class="col-9 col-sm-12">
+          <input
+            class="form-input"
+            type="text"
+            id="type"
+            v-model="type"
+            placeholder="type"
+          />
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-3 col-sm-12">
+          <label class="form-label" for="type">Date</label>
+        </div>
+        <div class="col-9 col-sm-12">
+          <input
+            class="form-input"
+            type="text"
+            id="date"
+            v-model="date"
+            placeholder="date"
+          />
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-12 col-sm-12">
+          <button class="btn btn-primary input-group-btn btn-lg">Submit</button>
+        </div>
+      </div>
+      <!-- form structure -->
+    </form>
+  </div>
 </template>
 
 <script>
@@ -32,7 +103,8 @@ export default {
         title: this.title,
         reasons: this.reasons,
         author: this.author,
-        type: this.type
+        type: this.type,
+        date: this.date
       };
       this.__submitToServer(decisionData);
     },
